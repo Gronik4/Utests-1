f4c25502af64a832ff1af3b03420d2871e567c6d
Gronik64 <golegni@yandex.ru>
2021-10-27 13:03:09 +0300

    Вторая правка CI

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,9 @@
 export default function healthLevel(item) {
     let level;
-    if(item.health >= 50) {
+    if (item.health >= 50) {
         level = 'healthy';
     } else {
-        if(item.health >= 15) {
+        if (item.health >= 15) {
             level = 'wounded';
         } else {
             level = 'critical';
