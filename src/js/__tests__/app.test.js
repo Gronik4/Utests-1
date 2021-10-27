adc4a51cc9e4ea213ebc0638c4a9155c165a649c
Gronik64 <golegni@yandex.ru>
2021-10-27 13:34:40 +0300

    Правка №3 CI

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,14 +1,14 @@
 import healthLevel from '../app';
 
 test.each([
-[{ name: 'Маг', health: 60 }, 'healthy'],
-[{ name: 'Маг', health: 40 }, 'wounded'],
-[{ name: 'Маг', health: 12 }, 'critical'],
+  [{ name: 'Маг', health: 60 }, 'healthy'],
+  [{ name: 'Маг', health: 40 }, 'wounded'],
+  [{ name: 'Маг', health: 12 }, 'critical'],
 ])(
-('Test level health'),
-(object, expected) => {
-const resalt = healthLevel(object);
+  ('Test level health'),
+  (object, expected) => {
+  const resalt = healthLevel(object);
 
-expect(resalt).toBe(expected);
-},
+  expect(resalt).toBe(expected);
+  },
 );
