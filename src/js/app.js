cf3de7cbc1fd49ae48f6de7ddf18e96a2d942442
Gronik64 <golegni@yandex.ru>
2021-10-27 14:10:38 +0300

    №7

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ export default function healthLevel(item) {
   if (item.health >= 50) {
     level = 'healthy';
     return level;
-  } 
+  }
   if (item.health >= 15) {
     level = 'wounded';
   } else {
